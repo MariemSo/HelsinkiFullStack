2743b3f499ac51bb3411a3645730c1128169a38b
MairemSo <souayah.mariem@gmail.com>
2024-12-31 03:52:52 +0100

    Exercice_2.11:ThePhonebookStep6

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,21 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Persons from "./components/Persons";
 import PersonsForm from "./components/PersonsForm";
 import Filter from "./components/Filter";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterPerson, setFilterPerson] = useState([]);
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log(response.data);
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
