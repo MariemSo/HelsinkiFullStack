a4a4767be65189d7afca79f3b89b07501087edad
MairemSo <souayah.mariem@gmail.com>
2025-01-03 20:16:46 +0100

    addingStylesToReactApp-Draft

--- part2/draft/src/App.jsx
+++ part2/draft/src/App.jsx
@@ -1,14 +1,31 @@
 // /* eslint-disable react/jsx-key */
 // /* eslint-disable react/prop-types */
-import "./App.css";
 import { useState, useEffect } from "react";
 import Note from "./components/Note";
 import noteService from "./services/notes";
+import Notification from "./components/Notification";
+
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, University of Helsinki 2024
+      </em>
+    </div>
+  );
+};
 
 function App() {
   const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("a new note ...");
+  const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -27,7 +44,12 @@ function App() {
       })
       .catch((error) => {
         console.log(error);
-        alert(`The note '${note.content}' was already deleted from server`);
+        setErrorMessage(
+          `The note '${note.content}' was already deleted from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
@@ -57,6 +79,7 @@ function App() {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -77,6 +100,7 @@ function App() {
         <input value={newNote} onChange={handleNoteChanges} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 }
